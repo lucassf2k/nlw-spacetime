870fe3c19f8955472ded02d1044a1623ffb722c7
lucassf2k <lucassilved@gmail.com>
2023-05-18 21:48:25 -0300

    create nextjs app

---
+++ front-spacetime/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-900 w-screen h-screen">
+      <h1 className="text-4xl font-bold text-slate-100">Hello World</h1>
+    </div>
+  );
+}
