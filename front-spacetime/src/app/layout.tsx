abc4e3e6972f4540b1a3e77041703190e9382029
Lucas Silva <lucassilved@gmail.com>
2023-05-27 14:12:41 -0300

    laout and authentication routes

--- front-spacetime/src/app/layout.tsx
+++ front-spacetime/src/app/layout.tsx
@@ -1,9 +1,15 @@
 import "./globals.css"
 import { ReactNode } from "react"
+import { cookies } from "next/headers"
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from "next/font/google"
+
+import { Hero } from "@/components/Hero"
+import { Copyright } from "@/components/Copyright"
+import { Profile } from "@/components/Profile"
+import { SignIn } from "@/components/SignIn"
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" })
 const baiJamjuree = BaiJamjuree({
@@ -19,12 +25,35 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has("token")
+
   return (
     <html lang="pt-BR">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {/* signin */}
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            {/* hero */}
+            <Hero />
+
+            {/* copyright */}
+            <Copyright />
+          </div>
+
+          {/* right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- front-spacetime/src/lib/api.ts
+++ front-spacetime/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://192.168.2.66:3001",
 })
